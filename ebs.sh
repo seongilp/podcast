74be584c5f432a4af8eff2442bcd906d1e54e025
Park Seongil <zihado@gmail.com>
2013-03-30 11:14:03 +0900

    Update ebs.sh

--- ebs.sh
+++ ebs.sh
@@ -2,9 +2,8 @@
 
 DATE=`date +%Y%m%d`
 PUBDATE=`date -R`
-#PUBDATE=`date "+%a, %d %b %Y %H:%M:%S %Z"`
-#EBSURL="mms://211.218.209.124/L-FM_300k"
 EBSURL="mms://ebslive.nefficient.com/ebswmslive"
+SERVERURL="http://ABC.com"
 FPATH="$HOME/EBS"
 FNAME="$1_$DATE"
 RECTIME=$2
@@ -45,7 +44,7 @@ echo "<itunes:author>EBS</itunes:author>" >> /var/www/ebs/ebs.xml
 echo "<itunes:subtitle>EBS Radio</itunes:subtitle>" >> /var/www/ebs/ebs.xml
 echo "<itunes:summary>EBS Radio</itunes:summary>" >> /var/www/ebs/ebs.xml
 echo "<enclosure url=\"http://zihado.com/ebs/$PODMP3\" length=\"$PODLEN\" type=\"audio/mpeg\" />" >> /var/www/ebs/ebs.xml
-echo "<guid>http://zihado.com/ebs/"$PODMP3"</guid>" >> /var/www/ebs/ebs.xml
+echo "<gu$SERVERURLebs/"$PODMP3"</guid>" >> /var/www/ebs/ebs.xml
 echo "<pubDate>"$PUBDATE"</pubDate>" >> /var/www/ebs/ebs.xml
 echo "<itunes:duration>"$2":00</itunes:duration>" >> /var/www/ebs/ebs.xml
 echo "<itunes:explicit>no</itunes:explicit>" >> /var/www/ebs/ebs.xml
